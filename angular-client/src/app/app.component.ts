a3c5a148eff24fb284c2c140ea860e98276eebc2
Doug Wilson <douglas.e.wilson@nasa.gov>
2020-04-03 07:52:15 -0400

    chore: adds user routing, creates home component, excludes Ivy in compiler options

--- angular-client/src/app/app.component.ts
+++ angular-client/src/app/app.component.ts
@@ -8,51 +8,5 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'app works!';
-
-  // Link to our api, pointing to localhost
-  API = 'http://localhost:3000';
-
-  // Declare empty list of people
-  people: any[] = [];
-  person: any;
-
-  constructor(private http: HttpClient) { }
-
-  // Angular 2 Life Cycle event when component has been initialized
-  ngOnInit() {
-    this.getAllPeople();
-  }
-
-  // Add one person to the API
-  public addPerson(name, age) {
-    this.http.post(`${this.API}/users`, { name, age })
-      .subscribe(() => {
-        this.getAllPeople();
-      });
-  }
-
-  // Get all users from the API
-  public getAllPeople() {
-    this.http.get(`${this.API}/users`)
-      .subscribe((people: any) => {
-        this.people = people;
-      });
-  }
-
-  // Find 1 person by ID
-
-  public findPerson(id) {
-    this.http.get(`${this.API}/users/${id}`).subscribe((person) => {
-      this.person = person;
-    });
-  }
-
-  // Delete 1 person by ID
-  public deletePerson(id) {
-    this.http.delete(`${this.API}/users/${id}`).subscribe((person) => {
-      this.person = person;
-      this.getAllPeople();
-    });
-  }
-
+  ngOnInit() {  }
 }

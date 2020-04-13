dec5dc3b4165e1d653980ca98921ffaf21886d81
Doug Wilson <dougwilson65@gmail.com>
2020-04-13 10:57:27 -0400

    Add address object and make api compatible; add some material design features (#31)

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -2,14 +2,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { SigninComponent } from './signin/signin.component';
-import { FormsModule } from '@angular/forms';
 import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
@@ -26,7 +31,13 @@ import { SignupComponent } from './signup/signup.component';
     HttpClientModule,
     MatButtonModule,
     MatToolbarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
     FormsModule
   ],
   providers: [HttpClientModule],
--- angular-client/src/app/users/users.component.ts
+++ angular-client/src/app/users/users.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
@@ -12,17 +13,36 @@ export class UsersComponent implements OnInit {
   // Declare empty list of users
   users: any[] = [];
   user: any;
+  userProfile = this.fb.group({
+    firstName: ['Doug', Validators.required],
+    lastName: ['Wilson', Validators.required],
+    dateOfBirth: ['1965-12-17'],
+    mobile: ['443-974-7038', [Validators.required, Validators.minLength(12)]],
+    email: ['dougwilson65@gmail.com'],
+    address: this.fb.group({
+      street: ['6078 Wild Ginger Ct.'],
+      city: ['Columbia'],
+      state: ['MD'],
+      zip: ['21044']
+    }),
+    aliases: this.fb.array([
+      this.fb.control('')
+    ])
+  });
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private fb: FormBuilder) { }
 
   // Angular 2 Life Cycle event when component has been initialized
   ngOnInit() {
     this.getAllUsers();
   }
 
+  get f() { return this.userProfile.controls; }
+
   // Add one user to the API
-  public addUser(name, age) {
-    this.http.post(`/api/users`, { name, age })
+  public addUser() {
+    console.log(this.f.mobile.value);
+    this.http.post(`/api/users`, { user: this.userProfile.value })
       .subscribe(() => {
         this.getAllUsers();
       });
@@ -46,8 +66,7 @@ export class UsersComponent implements OnInit {
 
   // Delete 1 user by ID
   public removeUser(id) {
-    this.http.delete(`/api/users/${id}`).subscribe((user) => {
-      this.user = user;
+    this.http.delete(`/api/users/${id}`).subscribe(() => {
       this.getAllUsers();
     });
   }
